eee08de742c6a42e0d1c11ddb6c4a2777404606b
Joy bhattacharya <joy.arya7381@gmail.com>
2022-02-09 23:40:40 +0530

    Project-Product management

--- src/index.js
+++ src/index.js
@@ -7,12 +7,11 @@ const route = require('./routes/route.js');
 const app = express();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(multer().any())
 
 app.use('/', route);
 
-mongoose.connect("mongodb+srv://users-open-to-all:hiPassword123@cluster0.uh35t.mongodb.net/Joy_Bhattacharya-DB?authSource=admin&replicaSet=atlas-wwe75z-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true", { useNewUrlParser: true })
+mongoose.connect("mongodb+srv://Joy-DB:joy123@cluster0.e8rbz.mongodb.net/Product_management", { useNewUrlParser: true })
     .then(() => console.log('DB connected'))
     .catch(err => console.log(err))
 
